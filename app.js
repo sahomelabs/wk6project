2a1eb178525c4b49c8f932f9f9a8e69939ab12f2
sahomelabs <sahomelabs@gmail.com>
2023-11-10 21:48:08 -0500

    creat game logic

---
+++ app.js
@@ -0,0 +1,22 @@
+// game logic 
+let danielCasinoGame = {
+    'player': {'scoreSpan': '#playerscore' , 
+    'div': '#player-score', 
+    'score': 0},
+
+    'dealer': {'scoreSpan': '#dealerscore' ,
+     'div': '#dealer-score', 
+     'score': 0},
+    
+
+
+    'cards': [],
+    
+    'cardsmap': {},
+
+    'wins': 0,
+    'lossese': 0,
+    'draws': 0,
+};
+
+
